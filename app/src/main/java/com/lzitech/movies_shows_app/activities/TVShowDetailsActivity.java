f574470e32b340dfa51bfaa5128865bf1a59474a
LoloziAnas <lolozianas1@gmail.com>
2021-10-03 23:13:27 +0100

    Adding Indicators to Image Slider

--- app/src/main/java/com/lzitech/movies_shows_app/activities/TVShowDetailsActivity.java
+++ app/src/main/java/com/lzitech/movies_shows_app/activities/TVShowDetailsActivity.java
@@ -1,12 +1,17 @@
 package com.lzitech.movies_shows_app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.lzitech.movies_shows_app.R;
@@ -49,6 +54,42 @@ public class TVShowDetailsActivity extends AppCompatActivity {
         activityTvshowDetailsBinding.sliderViewPager.setAdapter(new ImageSliderAdapter(sliderImages));
         activityTvshowDetailsBinding.sliderViewPager.setVisibility(View.VISIBLE);
         activityTvshowDetailsBinding.viewFadingEdge.setVisibility(View.VISIBLE);
+        setupSliderIndicators(sliderImages.length);
+        activityTvshowDetailsBinding.sliderViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentSliderIndicator(position);
+            }
+        });
+    }
 
+    private void setupSliderIndicators(int count) {
+        ImageView[] indicators = new ImageView[count];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(8, 0, 8, 0);
+        for (int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.background_slider_indicator_inactive));
+
+            indicators[i].setLayoutParams(layoutParams);
+            activityTvshowDetailsBinding.layoutSliderIndicators.addView(indicators[i]);
+        }
+        activityTvshowDetailsBinding.layoutSliderIndicators.setVisibility(View.VISIBLE);
+        //setCurrentSliderIndicator(0);
+    }
+
+    private void setCurrentSliderIndicator(int position) {
+        int childCount = activityTvshowDetailsBinding.layoutSliderIndicators.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView) activityTvshowDetailsBinding.layoutSliderIndicators.getChildAt(position);
+            if (i == position) {
+                imageView.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.background_slider_indicator_active));
+            } else {
+                imageView.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.background_slider_indicator_inactive));
+            }
+        }
     }
 }
